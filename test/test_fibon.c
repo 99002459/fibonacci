d0d89a36f632d6e55c9807e9bad6a1213b7891cb
stepin104300 <69077154+stepin104300@users.noreply.github.com>
2020-09-18 15:29:06 +0530

    Update test_fibon.c

--- test/test_fibon.c
+++ test/test_fibon.c
@@ -1,23 +1,39 @@
 
-#include "unity.h"
+
 #include "fibon.h"
+#include <CUnit/Basic.h>
+#include <CUnit/CUnit.h>
 
 	void setUp()
 	{	}
 	void tearDown()
 	{	}
-
-	void test_fibon(void)
+void test_fibon(void)
 	{
-  		TEST_ASSERT_EQUAL(1, fibon(8));
-  		TEST_ASSERT_EQUAL(0, fibon(10));
+  		CU_ASSERT_EQUAL(1== fibon(8));
+  		CU_ASSERT_EQUAL(0== fibon(10));
 	}
+	
 
-	int test_main(void)
-	{
-	  	UNITY_BEGIN();
+	int main() {
+/* Note: Do not edit START*/
+  /*Initialize and setup the Test Framework */
+  if (CUE_SUCCESS != CU_initialize_registry())
+    return CU_get_error();
+  CU_pSuite suite = CU_add_suite(PROJECT_NAME, 0, 0);
+/* Note: Do not edit END */
+  
+ 
 
-	  	RUN_TEST(test_fibon);
-
-  		return UNITY_END();
-	}
+/* Note: Do not edit START*/
+  /* Setup Test Framework to output the result to Screen */
+  CU_basic_set_mode(CU_BRM_VERBOSE);
+  
+  /* run the unit test framework*/
+  CU_basic_run_tests();
+  
+  /* Cleaning the Resources used by Unit test framework */
+  CU_cleanup_registry();
+/* Note: Do not edit END */
+  return 0;
+}
