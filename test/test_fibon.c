257911cd94dde89df6620db289c8cb15fe23c9a4
stepin104300 <69077154+stepin104300@users.noreply.github.com>
2020-09-18 18:08:58 +0530

    Add files via upload

--- test/test_fibon.c
+++ test/test_fibon.c
@@ -1,40 +1,23 @@
 
+#include "unity.h"
+#include "fibon.h"
 
+	void setUp()
+	{	}
+	void tearDown()
+	{	}
 
-#include <CUnit/Basic.h>
-#include <CUnit/CUnit.h>
-
-#include <fibon.h>
-#define PROJECT_NAME    "fibonacci"
-
-void test_fibon(void);
-	
-
-	int main() {
-/* Note: Do not edit START*/
-  /*Initialize and setup the Test Framework */
-  if (CUE_SUCCESS != CU_initialize_registry())
-    return CU_get_error();
-  CU_pSuite suite = CU_add_suite(PROJECT_NAME, 0, 0);
-/* Note: Do not edit END */
-  
-  CU_add_test(suite, "fibon", test_fibon);
-
-/* Note: Do not edit START*/
-  /* Setup Test Framework to output the result to Screen */
-  CU_basic_set_mode(CU_BRM_VERBOSE);
-  
-  /* run the unit test framework*/
-  CU_basic_run_tests();
-  
-  /* Cleaning the Resources used by Unit test framework */
-  CU_cleanup_registry();
-/* Note: Do not edit END */
-  return 0;
-}
-void test_fibon(void)
+	void test_fibon(void)
 	{
-  		CU_ASSERT(1==fibon(8));
-  		CU_ASSERT(0==fibon(10));
+  		TEST_ASSERT_EQUAL(1, fibon(8));
+  		TEST_ASSERT_EQUAL(0, fibon(10));
 	}
-	
+
+	int test_main(void)
+	{
+	  	UNITY_BEGIN();
+
+	  	RUN_TEST(test_fibon);
+
+  		return UNITY_END();
+	}
